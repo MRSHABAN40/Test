f88f7413ed86e86dfb49fdcd550b43b1ebd92449
shani-kh <misbhakhan3788@gmail.com>
2025-03-12 17:12:37 +0500

    Update presence.js

--- plugins/presence.js
+++ plugins/presence.js
@@ -27,7 +27,7 @@ async (conn, mek, m, { from, body, isOwner }) => {
     for (const text in data) {
         if (body.toLowerCase() === text.toLowerCase()) {
             
-            if (config.AUTO_VOICE === 'true') {
+            if (config.AUTO_VOICE === 'false') {
                 //if (isOwner) return;        
                 await conn.sendPresenceUpdate('recording', from);
                 await conn.sendMessage(from, { audio: { url: data[text] }, mimetype: 'audio/mpeg', ptt: true }, { quoted: mek });
@@ -47,7 +47,7 @@ async (conn, mek, m, { from, body, isOwner }) => {
     for (const text in data) {
         if (body.toLowerCase() === text.toLowerCase()) {
             
-            if (config.AUTO_STICKER === 'true') {
+            if (config.AUTO_STICKER === 'false') {
                 //if (isOwner) return;        
                 await conn.sendMessage(from,{sticker: { url : data[text]},package: 'SHABAN-SOBX-MD'},{ quoted: mek })   
             
